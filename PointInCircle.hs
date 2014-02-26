e3bf3898fcff629f1a7f3572673607a92b817f3e
Bruno <bacm@live.co.uk>
2014-02-26 18:39:42 +0100

    Create PointInCircle.hs

---
+++ PointInCircle.hs
@@ -0,0 +1,22 @@
+import System.Environment (getArgs)
+import Text.Regex.Base
+
+f :: String -> Double
+f s = read (s)
+matched l = l =~ "[0-9.-]+" :: [[String]]
+
+transform :: Double -> Double -> Double -> Double -> Double -> Bool 
+transform cx cy radius x y = ((x) - (cx))^2 + ((y) - (cy))^2 < (radius)^2
+
+isinside :: [[String]] -> String 
+isinside ([cx]:[cy]:[radius]:[x]:[y]:[]) 
+        | transform (f cx) (f cy) (f radius) (f x) (f y) = "true"
+        | otherwise = "false"
+
+check :: String -> String 
+check line = isinside $ matched line
+
+main = do
+        [inpFile] <- getArgs
+        input <- readFile inpFile
+        mapM_ putStrLn $ map check $ lines input
